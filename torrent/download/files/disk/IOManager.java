94f294b6988d13dff0d8c101657cab8dadf56b3b
Johnnei <johnnei07@live.nl>
2013-01-19 14:33:55 +0100

    Started with IOManager

---
+++ torrent/download/files/disk/IOManager.java
@@ -0,0 +1,39 @@
+package torrent.download.files.disk;
+
+import java.util.PriorityQueue;
+
+import torrent.download.Torrent;
+
+public class IOManager {
+	
+	private PriorityQueue<DiskJob> taskQueue;
+	
+	public IOManager() {
+		taskQueue = new PriorityQueue<>();
+	}
+	
+	/**
+	 * Adds a task to the queue
+	 * @param task The task to add
+	 */
+	public void addTask(DiskJob task) {
+		synchronized (this) {
+			taskQueue.add(task);
+		}
+	}
+	
+	/**
+	 * Processes a single task for the given torrent
+	 * @param torrent
+	 */
+	public void processTask(Torrent torrent) {
+		if(taskQueue.size() > 0) {
+			DiskJob task;
+			synchronized (this) {
+				task = taskQueue.remove();
+			}
+			task.process(torrent);
+		}
+	}
+
+}
