772dc69a82c559be4567eb47e1820f0ae2c4e5ed
Johnnei <johnnei07@live.nl>
2013-03-15 19:40:25 +0100

    Added all Unittesting testcases

--- unittesting/BST.java
+++ unittesting/BST.java
@@ -9,7 +9,7 @@ import torrent.util.tree.BinarySearchTree;
 public class BST {
 
 	@Test
-	public void test() {
+	public void leftRotation() {
 		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 		bst.add(12);
 		bst.add(16);
@@ -21,7 +21,53 @@ public class BST {
 		bst.add(6);
 		bst.add(1);
 		bst.printInOrder();
-		assertTrue("AVL Property", bst.isAVL());
+		assertTrue("AVL Property (Left Rotation)", bst.isAVL());
+	}
+	@Test
+	public void rightRotation() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		bst.add(-12);
+		bst.add(-16);
+		bst.add(-8);
+		bst.add(-4);
+		bst.add(-14);
+		bst.add(-10);
+		bst.add(-2);
+		bst.add(-6);
+		bst.add(-1);
+		bst.printInOrder();
+		assertTrue("AVL Property (Right Rotation)", bst.isAVL());
+	}
+	
+	@Test
+	public void leftDoubleRotation() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		bst.add(12);
+		bst.add(16);
+		bst.add(8);
+		bst.add(4);
+		bst.add(14);
+		bst.add(10);
+		bst.add(2);
+		bst.add(6);
+		bst.add(5);
+		bst.printInOrder();
+		assertTrue("AVL Property (Left Double Rotation)", bst.isAVL());
+	}
+	@Test
+	public void rightDoubleRotation() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		bst.add(-12);
+		bst.add(-16);
+		bst.add(-8);
+		bst.add(-4);
+		bst.add(-14);
+		bst.add(-10);
+		bst.add(-2);
+		bst.add(-6);
+		bst.add(-5);
+		bst.printInOrder();
+		assertTrue("AVL Property (Right Double Rotation)", bst.isAVL());
 	}
 
 }
